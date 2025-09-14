17a4d4f63db45f16b9809896fa1f675bfdb1265e
EricNohara <ernohara@bu.edu>
2025-09-14 15:26:28 -0400

    Add Textbook programming problem projects

---
+++ HeadsOrTails-Textbook-PP5/Form1.cs
@@ -0,0 +1,10 @@
+namespace HeadsOrTails_Textbook_PP5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LatinTranslator-Textbook-PP1/Form1.cs
@@ -0,0 +1,10 @@
+namespace LatinTranslator_Textbook_PP1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
